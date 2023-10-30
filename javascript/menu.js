1ce9ca1ec1d1086847fc034a1b14cef7d4ebd0f5
Marllonlp <marlonmota2019@gmail.com>
2023-10-30 17:58:39 -0300

    commit

--- javascript/menu.js
+++ javascript/menu.js
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 var rodape = `
 <footer class="bg-light text-lg-start">
-        <div class="container p-4">
+        <div class="container-fluid p-4">
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <h5>Sobre Nós</h5>
